abd200be0e72796e1e9d200412dc5767ce892873
Brent Reynolds <brentsreynolds5@gmail.com>
2024-05-06 22:26:54 -0400

    first commit

---
+++ AlgorithmsVisualizerBlazor/Algorithms/Algorithms.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsVisualizerBlazor.Algorithms
+{
+    public class Algorithms
+    {
+        public static void SelectionSort(List<int> numbers)
+        {
+            int listLength = numbers.Count;
+            for (int i = 0; i < listLength - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < listLength; j++)
+                {
+                    if (numbers[j] < numbers[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp = numbers[minIndex];
+                numbers[minIndex] = numbers[i];
+                numbers[i] = temp;
+            }
+        }
+
+        public static void BubbleSort(List<int> numbers)
+        {
+            int listLength = numbers.Count;
+            for (int i = 0; i < listLength - 1; i++)
+            {
+                for (int j = 0; j < listLength - i - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        public static void InsertionSort(List<int> numbers)
+        {
+            int listLength = numbers.Count;
+            for (int i = 1; i < listLength; i++)
+            {
+                int key = numbers[i];
+                int flag = 0;
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (key < numbers[j])
+                    {
+                        numbers[j + 1] = numbers[j];
+                    }
+                    else
+                    {
+                        numbers[j + 1] = key;
+                        flag = 1;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public static void MergeSort(int[] arr)
+        {
+            if (arr.Length <= 1)
+                return;
+
+            int mid = arr.Length / 2;
+            int[] left = new int[mid];
+            int[] right = new int[arr.Length - mid];
+
+            for (int i = 0; i < mid; i++)
+                left[i] = arr[i];
+            for (int i = mid; i < arr.Length; i++)
+                right[i - mid] = arr[i];
+
+            MergeSort(left);
+            MergeSort(right);
+
+            Merge(arr, left, right);
+        }
+
+        private static void Merge(int[] arr, int[] left, int[] right)
+        {
+            int i = 0, j = 0, k = 0;
+            while (i < left.Length && j < right.Length)
+            {
+                if (left[i] <= right[j])
+                    arr[k++] = left[i++];
+                else
+                    arr[k++] = right[j++];
+            }
+
+            while (i < left.Length)
+                arr[k++] = left[i++];
+            while (j < right.Length)
+                arr[k++] = right[j++];
+        }
+
+        public static string ListToString(List<int> numbers)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (int number in numbers)
+                sb.Append(number + " ");
+            return sb.ToString();
+        }
+
+        public static string ArrayToString(int[] array)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (int number in array)
+                sb.Append(number + " ");
+            return sb.ToString();
+        }
+    }
+}
+
